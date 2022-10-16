6a91aefe82a0d7f3a50df8594fe314a79e71658e
blcslk <115918743+blcslk@users.noreply.github.com>
2022-10-16 21:00:32 +0200

    Update KEY_langfix.sh

--- KEY_langfix.sh
+++ KEY_langfix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# not working: use -> under final KEY set !!!
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 536F8F1DE80F6A35
 sudo apt-get update
@@ -23,7 +24,7 @@ sudo apt install nano -y >/dev/null
 #LANG="C"
 ##LANG=C perl -e exit
 
-
+# not working: use -> error messages all matching KEY delete !!!
 sudo apt update
 sudo apt remove --purge ubuntu-dbgsym-keyring
 sudo apt-key del 0xC8CAB6595FDFF622
@@ -48,7 +49,7 @@ cat /etc/locale.gen | grep -v "#"
 ##export LC_ALL="en_US.UTF-8"
 
 localefix01 () {
-#### HARDY METHOD ####
+### IF JUST NOT WORKING THIS IS "UBUNTU METHOD" ###
 sudo locale-gen en_US.UTF-8
 sudo localedef -i en_US -f UTF-8 en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
@@ -61,7 +62,7 @@ sudo dpkg-reconfigure locales
 #localefix01
 
 localefic02 () {
-### JUST NOT WORK: (ubuntu) ###
+### UBUNTU METHOD ###
 sudo locale-gen en_US.UTF-8
 LANG=en_US.UTF-8
 sudo update-locale LANG=en_US.UTF-8
