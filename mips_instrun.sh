a45e6247fbc29c969943540baa250a3255008732
blcslk <115918743+blcslk@users.noreply.github.com>
2022-10-16 08:28:00 +0000

    Update mips_instrun.sh

--- mips_instrun.sh
+++ mips_instrun.sh
@@ -12,7 +12,7 @@ sudo screen --help >/dev/null 2>&1 || sudo apt install qemu-system-mips screen -
 [ `ps aux | grep debian.*.mips | sed -n '2p' | wc -l` = '1' ] && sudo kill "$(ps aux | grep debian.*.mips | awk '{print $2}' | sed -n '1p')"
 [ `pidof linux_mips | wc -l` = '0' ] || sudo kill `pidof linux_mips`
 
-sudo qemu-system-mips -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mips_standard.qcow2 -append "root=/dev/sda1" -nographic -redir tcp:2222::22' -m 1024 >/dev/null 2>&1 &
+sudo qemu-system-mips -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mips_standard.qcow2 -append "root=/dev/sda1" -nographic -redir tcp:2222::22 -m 1024 >/dev/null 2>&1 &
 
 #kill "$(ps aux | grep debian.*.mips | awk '{print $2}' | sed -n '1p')"
 echo -e 'use -> kill "'"\$(ps aux | grep debian.*.mips | awk '{print \$2}' | sed -n '1p'"')"'
