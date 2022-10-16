9c8ed0bd1c3bb64cf22e639c54a14c3d48d6bde2
blcslk <115918743+blcslk@users.noreply.github.com>
2022-10-16 08:25:23 +0000

    Add files via upload

---
+++ KEY_langfix.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 536F8F1DE80F6A35
+sudo apt-get update
+sudo apt remove --purge ubuntu-dbgsym-keyring
+sudo apt-key del 0x172B5989FCD21EF8
+sudo apt install ubuntu-dbgsym-keyring
+sudo apt install --reinstall ubuntu-dbgsym-keyring
+
+#sudo localectl set-locale LANG=en_US.UTF-8
+
+sudo apt install nano -y >/dev/null
+
+#sudo nano /etc/locale.gen
+##sudo localectl set LC_ALL="en_US.UTF-8"
+##LANG=C perl -e exit
+##env LANG=C perl -e exit
+##LC_MESSAGES="en_US.UTF-8"
+##LANGUAGE="en_US.UTF-8"
+##LC_ALL="en_US.UTF-8"
+##LC_CTYPE="en_US.UTF-8"
+#LANG="C"
+##LANG=C perl -e exit
+
+
+sudo apt update
+sudo apt remove --purge ubuntu-dbgsym-keyring
+sudo apt-key del 0xC8CAB6595FDFF622
+sudo apt-key del 0x172B5989FCD21EF8
+sudo apt install ubuntu-dbgsym-keyring
+sudo locale-gen
+sudo apt install --reinstall ubuntu-dbgsym-keyring
+sudo locale-gen
+
+sudo apt-get update
+sudo apt-key del  0x172B5989FCD21EF8
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+uname -a
+sudo apt update
+sudo apt-get update
+sudo locale-gen
+
+echo -e '\nlanguage check:'
+cat /etc/locale.gen | grep -v "#"
+
+##export LC_CTYPE="en_US.UTF-8"
+##export LC_ALL="en_US.UTF-8"
+
+localefix01 () {
+#### HARDY METHOD ####
+sudo locale-gen en_US.UTF-8
+sudo localedef -i en_US -f UTF-8 en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+sudo locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+#exit
+}
+#localefix01
+
+localefic02 () {
+### JUST NOT WORK: (ubuntu) ###
+sudo locale-gen en_US.UTF-8
+LANG=en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8
+LANG=en_US.UTF-8
+LC_MESSAGES=POSIX
+sudo locale -a
+sudo update-locale LANG=en_US.UTF-8
+#exit
+}
+localefix02
+
+### debconf fail PROBLEM FIX ###
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+sudo apt-get install -y -q
+sudo chmod 777 /var/cache/debconf/
+sudo chmod 777 /var/cache/debconf/passwords.dat
+
+echo -e "\nclose terminal and again new open terminal, without system restart !!!\n"
---
+++ mips_instrun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo qemu-system-mips --help >/dev/null 2>&1 || sudo apt install qemu-system-mips screen -y >/dev/null 2>&1
+sudo screen --help >/dev/null 2>&1 || sudo apt install qemu-system-mips screen -y >/dev/null 2>&1
+
+#[ -z `pwd | grep 'linux_mips'` ] && cd linux_mips
+[ -d linux_mips ] || mkdir linux_mips && cd linux_mips
+
+[ -f vmlinux* ] || wget https://people.debian.org/~aurel32/qemu/mips/vmlinux-3.2.0-4-4kc-malta >/dev/null 2>&1
+[ -f debian_wheezy_mips_standard* ] || wget https://people.debian.org/~aurel32/qemu/mips/debian_wheezy_mips_standard.qcow2 >/dev/null 2>&1
+
+[ `ps aux | grep debian.*.mips | sed -n '2p' | wc -l` = '1' ] && sudo kill "$(ps aux | grep debian.*.mips | awk '{print $2}' | sed -n '1p')"
+[ `pidof linux_mips | wc -l` = '0' ] || sudo kill `pidof linux_mips`
+
+sudo qemu-system-mips -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mips_standard.qcow2 -append "root=/dev/sda1" -nographic -redir tcp:2222::22' -m 1024 >/dev/null 2>&1 &
+
+#kill "$(ps aux | grep debian.*.mips | awk '{print $2}' | sed -n '1p')"
+echo -e 'use -> kill "'"\$(ps aux | grep debian.*.mips | awk '{print \$2}' | sed -n '1p'"')"'
+echo -e '\nuse -> ssh -P 2222 root@127.0.0.1\n'
